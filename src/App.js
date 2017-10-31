fbf4b1aed48498f9d7de4391f96931722754547b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-31 14:12:28 +0700

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 
-const App = () => null;
+const App = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>My Contacts</h1>
+  </header>
+);
 
 export default App;

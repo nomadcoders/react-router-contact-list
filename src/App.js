11a46dccaba1ca2819c9e308fcb4bc5a2f021471
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-31 14:18:32 +0700

    Created Home page  + Contacts

--- src/App.js
+++ src/App.js
@@ -10,7 +10,22 @@ const App = () => (
 const Header = () => (
   <header>
     <h1>My Contacts</h1>
+    <Route exact path="/" component={Welcome} />
+    <Route path="/contacts" component={Contacts} />
   </header>
+);
+
+const Welcome = () => (
+  <h3>Welcome to the best contact app in the world. ever. always</h3>
+);
+
+const Contacts = () => (
+  <ul>
+    <li>Lynn</li>
+    <li>Japan Guy</li>
+    <li>Jisu Kimchi</li>
+    <li>Juan Malakas</li>
+  </ul>
 );
 
 export default App;
